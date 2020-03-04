29bc8b9a504fb8a98ec8d601111d8c22e8d21f38
Teyssier <theo.teyssier@etu.univ-grenoble-alpes.fr>
2020-03-04 17:32:23 +0100

    Test copy+swap

--- examples/test_swap.c
+++ examples/test_swap.c
@@ -27,15 +27,25 @@ int main (int argc, char **argv)
      cd2[idx] = (complexe_double_t){1.0,1.0};
  }
 
- printf("===TEST mncblas_ccopy()===\n");
+ printf("===TEST mncblas_cswap()===\n");
+ printf("c1 avant : [");
+ for (int idx=0; idx<TAILLE; idx++) {
+     printf("{%.0f %.0f}", c1[idx].real, c1[idx].imaginary);
+ }
+ printf("]\n");
  printf("c2 avant : [");
  for (int idx=0; idx<TAILLE; idx++) {
      printf("{%.0f %.0f}", c2[idx].real, c2[idx].imaginary);
  }
  printf("]\n");
 
- mncblas_ccopy(TAILLE, c1, 1, c2, 1);
+ mncblas_cswap(TAILLE, c1, 1, c2, 1);
 
+ printf("c1 apres : [");
+ for (int idx=0; idx<TAILLE; idx++) {
+     printf("{%.0f %.0f}", c1[idx].real, c1[idx].imaginary);
+ }
+ printf("]\n");
  printf("c2 apres : [");
  for (int idx=0; idx<TAILLE; idx++) {
      printf("{%.0f %.0f}", c2[idx].real, c2[idx].imaginary);
@@ -43,15 +53,25 @@ int main (int argc, char **argv)
  printf("]\n");
 
 
-printf("\n===TEST mncblas_zcopy()===\n");
+printf("\n===TEST mncblas_zswap()===\n");
+ printf("cd1 avant : [");
+ for (int idx=0; idx<TAILLE; idx++) {
+     printf("{%.0f %.0f}", cd1[idx].real, cd1[idx].imaginary);
+ }
+ printf("]\n");
  printf("cd2 avant : [");
  for (int idx=0; idx<TAILLE; idx++) {
      printf("{%.0f %.0f}", cd2[idx].real, cd2[idx].imaginary);
  }
  printf("]\n");
 
- mncblas_zcopy(TAILLE, cd1, 1, cd2, 1);
+ mncblas_zswap(TAILLE, cd1, 1, cd2, 1);
 
+ printf("cd1 apres : [");
+ for (int idx=0; idx<TAILLE; idx++) {
+     printf("{%.0f %.0f}", cd1[idx].real, cd1[idx].imaginary);
+ }
+ printf("]\n");
  printf("cd2 apres : [");
  for (int idx=0; idx<TAILLE; idx++) {
      printf("{%.0f %.0f}", cd2[idx].real, cd2[idx].imaginary);
@@ -60,45 +80,45 @@ printf("\n===TEST mncblas_zcopy()===\n");
 
 
 
- printf("===TEST perf mncblas_scopy===\n");
+ printf("===TEST perf mncblas_sswap===\n");
  float f1[TAILLE];
  float f2[TAILLE];
  init_flop () ;
  start =_rdtsc () ;
  for (i = 0 ; i < NB_FOIS; i++) {
-     mncblas_scopy(TAILLE, f1, 1, f2, 1);
+     mncblas_sswap(TAILLE, f1, 1, f2, 1);
  }
  end = _rdtsc () ;
  printf ("apres boucle %lld cycles \n\n", end-start) ;
 
 
- printf("===TEST perf mncblas_dcopy===\n");
+ printf("===TEST perf mncblas_dswap===\n");
  double d1[TAILLE];
  double d2[TAILLE];
  init_flop () ;
  start =_rdtsc () ;
  for (i = 0 ; i < NB_FOIS; i++) {
-     mncblas_dcopy(TAILLE, d1, 1, d2, 1);
+     mncblas_dswap(TAILLE, d1, 1, d2, 1);
  }
  end = _rdtsc () ;
  printf ("apres boucle %lld cycles \n\n", end-start) ;
 
 
- printf("===TEST perf mncblas_ccopy===\n");
+ printf("===TEST perf mncblas_cswap===\n");
  init_flop () ;
  start =_rdtsc () ;
  for (i = 0 ; i < NB_FOIS; i++) {
-     mncblas_ccopy(TAILLE, c1, 1, c2, 1);
+     mncblas_cswap(TAILLE, c1, 1, c2, 1);
  }
  end = _rdtsc () ;
  printf ("apres boucle %lld cycles \n\n", end-start) ;
 
 
- printf("===TEST perf mncblas_ccopy===\n");
+ printf("===TEST perf mncblas_cswap===\n");
  init_flop () ;
  start =_rdtsc () ;
  for (i = 0 ; i < NB_FOIS; i++) {
-     mncblas_ccopy(TAILLE, cd1, 1, cd2, 1);
+     mncblas_cswap(TAILLE, cd1, 1, cd2, 1);
  }
  end = _rdtsc () ;
  printf ("apres boucle %lld cycles \n\n", end-start) ;
