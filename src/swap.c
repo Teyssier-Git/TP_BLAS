e01b1f31d6c5b96e9f04e6f8e44279b5a0c2ad25
Teyssier-Git <58296448+Teyssier-Git@users.noreply.github.com>
2020-03-04 15:30:42 +0100

    Update swap.c

--- src/swap.c
+++ src/swap.c
@@ -1,12 +1,12 @@
 #include "mnblas.h"
 
-void mncblas_sswap(const int N, float *X, const int incX, 
+void mncblas_sswap(const int N, float *X, const int incX,
                  float *Y, const int incY)
 {
   register unsigned int i = 0 ;
   register unsigned int j = 0 ;
   register float save ;
-  
+
   for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
     {
       save = Y [j] ;
@@ -17,13 +17,13 @@ void mncblas_sswap(const int N, float *X, const int incX,
   return ;
 }
 
-void mncblas_dswap(const int N, double *X, const int incX, 
+void mncblas_dswap(const int N, double *X, const int incX,
                  double *Y, const int incY)
 {
   register unsigned int i = 0 ;
   register unsigned int j = 0 ;
   register double save ;
-  
+
   for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
     {
       save = Y [j] ;
@@ -33,35 +33,40 @@ void mncblas_dswap(const int N, double *X, const int incX,
   return ;
 }
 
-void mncblas_cswap(const int N, void *X, const int incX, 
+void mncblas_cswap(const int N, void *X, const int incX,
 		                    void *Y, const int incY)
 {
   register unsigned int i = 0 ;
   register unsigned int j = 0 ;
-  register void *save ;
-  
+  register void **save ;
+
+  void **x = (void **)X;
+  void **y = (void **)Y;
+
   for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
     {
-      save = Y [j] ;
-      Y [j] = X [i] ;
-      X [i] = save ;
+      save = y [j] ;
+      y [j] = x [i] ;
+      x [i] = save ;
     }
   return ;
 }
 
-void mncblas_zswap(const int N, void *X, const int incX, 
+void mncblas_zswap(const int N, void *X, const int incX,
 		                    void *Y, const int incY)
 {
   register unsigned int i = 0 ;
   register unsigned int j = 0 ;
-  register void *save ;
-  
+  register void **save ;
+
+  void **x = (void **)X;
+  void **y = (void **)Y;
+
   for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
     {
-      save = Y [j] ;
-      Y [j] = X [i] ;
-      X [i] = save ;
+      save = y [j] ;
+      y [j] = x [i] ;
+      x [i] = save ;
     }
   return ;
 }
-
