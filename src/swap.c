f1cb257db72467dbbcd218a4f856d6bb7d14643b
portieba <Barnabe-Portier@etu-univ-grenoble-alpes.fr>
2020-03-04 15:06:48 +0100

    swap.c implemented

--- src/swap.c
+++ src/swap.c
@@ -20,21 +20,48 @@ void mncblas_sswap(const int N, float *X, const int incX,
 void mncblas_dswap(const int N, double *X, const int incX, 
                  double *Y, const int incY)
 {
-
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  register double save ;
+  
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      save = Y [j] ;
+      Y [j] = X [i] ;
+      X [i] = save ;
+    }
   return ;
 }
 
 void mncblas_cswap(const int N, void *X, const int incX, 
 		                    void *Y, const int incY)
 {
-
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  register void *save ;
+  
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      save = Y [j] ;
+      Y [j] = X [i] ;
+      X [i] = save ;
+    }
   return ;
 }
 
 void mncblas_zswap(const int N, void *X, const int incX, 
 		                    void *Y, const int incY)
 {
-
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  register void *save ;
+  
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      save = Y [j] ;
+      Y [j] = X [i] ;
+      X [i] = save ;
+    }
   return ;
 }
 
