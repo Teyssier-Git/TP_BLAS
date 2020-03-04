fac9de420401fcd9f68d0cd340c335e63e6762b4
Teyssier-Git <58296448+Teyssier-Git@users.noreply.github.com>
2020-03-04 15:21:18 +0100

    Test copy 2

--- examples/test_copy.c
+++ examples/test_copy.c
@@ -14,41 +14,64 @@ int main (int argc, char **argv)
  complexe_float_t c1[TAILLE];
  complexe_float_t c2[TAILLE];
 
- // complexe_double_t cd1[TAILLE] ;
- // complexe_double_t cd2[TAILLE] ;
+ complexe_double_t cd1[TAILLE] ;
+ complexe_double_t cd2[TAILLE] ;
 
- // unsigned long long int start, end ;
- // int i ;
+ unsigned long long int start, end ;
+ int i ;
 
  for (int idx = 0; idx<TAILLE; idx++) {
      c1[idx] = (complexe_float_t){0.0,0.0};
+     cd1[idx] = (complexe_double_t){0.0,0.0};
+     c2[idx] = (complexe_float_t){1.0,1.0};
+     cd2[idx] = (complexe_double_t){1.0,1.0};
  }
 
- printf("c2\n");
- for (int idx=0; idx<TAILLE; idx++) {
-     printf("%.0f %.0f\n", c2[idx].real, c2[idx].imaginary);
+ for (int idx = 0; idx<TAILLE; idx++) {
+
  }
+
+ printf("c2 avant\n [");
+ for (int idx=0; idx<TAILLE; idx++) {
+     printf("{%.0f %.0f}", c2[idx].real, c2[idx].imaginary);
+ }
+ printf("]\n");
+
  mncblas_ccopy(TAILLE, c1, 1, c2, 1);
- printf("\nc2\n");
+
+ printf("\nc2 apres\n [");
  for (int idx=0; idx<TAILLE; idx++) {
-     printf("%.0f %.0f\n", c2[idx].real, c2[idx].imaginary);
+     printf("{%.0f %.0f}", c2[idx].real, c2[idx].imaginary);
  }
- // init_flop () ;
- //
- // printf ("cd3.r %f cd3.i %f\n", cd3.real, cd3.imaginary) ;
- //
- // start =_rdtsc () ;
- //
- // for (i = 0 ; i < NB_FOIS; i++)
- //   {
- //     mncblas_ccopy(const int N, const void *X, const int incX,
- //     		                    void *Y, const int incY)
- //   }
- //
- // end = _rdtsc () ;
- //
- //  printf ("apres boucle cd3.real %f cd3.imaginary %f %lld cycles \n", cd3.real, cd3.imaginary, end-start) ;
- //
- //  calcul_flop ("calcul copy ", NB_FOIS*4, end-start) ;
+ printf("]\n");
+
+ printf("cd2 avant\n [");
+ for (int idx=0; idx<TAILLE; idx++) {
+     printf("{%.0f %.0f}", cd2[idx].real, cd2[idx].imaginary);
+ }
+ printf("]\n");
+
+ mncblas_zcopy(TAILLE, cd1, 1, cd2, 1);
+
+ printf("\ncd2 apres\n [");
+ for (int idx=0; idx<TAILLE; idx++) {
+     printf("{%.0f %.0f}", c2[idx].real, c2[idx].imaginary);
+ }
+ printf("]\n");
+
+ init_flop () ;
+
+ start =_rdtsc () ;
+
+ for (i = 0 ; i < NB_FOIS; i++)
+   {
+     mncblas_ccopy(TAILLE, c1, 1, c2, 1);
+   }
+
+ end = _rdtsc () ;
+
+  printf ("apres boucle cd3.real %f cd3.imaginary %f %lld cycles \n", c1[0].real, c1[0].imaginary, end-start) ;
+
+  calcul_flop ("calcul copy ", NB_FOIS*4, end-start) ;
   exit (0) ;
 }
