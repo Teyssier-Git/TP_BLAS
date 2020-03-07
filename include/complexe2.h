182c46fbc7bd67f2b6ae6024ae74c7740ac26899
nguyejus <60656151+nguyejus@users.noreply.github.com>
2020-03-07 11:15:05 +0100

    Update complexe2.h

--- include/complexe2.h
+++ include/complexe2.h
@@ -47,7 +47,7 @@ inline complexe_double_t mult_complexe_double (const complexe_double_t c1, const
 
   r.real = (c1.real*c2.real) - (c1.imaginary*c2.imaginary) ;
   r.imaginary = (c1.real*c2.imaginary) + (c1.imaginary*c2.real)  ;
-  
+
   return r ;
 }
 
@@ -65,6 +65,7 @@ inline complexe_float_t div_complexe_float (const complexe_float_t c1, const com
   }
   else{
     printf("division par 0!");
+    return r;
   }
 
 
@@ -82,5 +83,6 @@ inline complexe_double_t div_complexe_double (const complexe_double_t c1, const 
   }
   else{
     printf("division par 0!");
+    return r;
   }
 }
