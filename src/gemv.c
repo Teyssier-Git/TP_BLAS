0c4f70caa76e7f1a07117ee3085cc0809b0a1420
Theo <theo26200@gmail.com>
2020-06-10 12:18:21 +0200

    Add parallel

--- src/gemv.c
+++ src/gemv.c
@@ -8,42 +8,46 @@ void mncblas_sgemv(const MNCBLAS_LAYOUT layout,
                  const float *X, const int incX, const float beta,
                  float *Y, const int incY) {
 
-    register unsigned int i, j;
+    register unsigned int i;
 
     if (TransA == MNCblasNoTrans) {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < M ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < (M/incX+1) ; i ++) {
                 float resLine = 0.0;
                 for (int k=0; k<N; k++) {
-                    resLine += A[N*i+k]*X[k];
+                    resLine += A[N*i*incX+k]*X[k];
                 }
-                Y[j] = alpha * resLine + beta * Y[j];
+                Y[i*incY] = alpha * resLine + beta * Y[i*incY];;
             }
         } else {
-            for (i = 0, j = 0 ; i < M ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < M/incX+1; i++) {
                 float resLine = 0.0;
                 for (int k=0; k<N; k++) {
-                    resLine += A[M*k+i]*X[k];
+                    resLine += A[M*k+i*incX]*X[k];
                 }
-                Y[j] = alpha * resLine + beta * Y[j];
+                Y[i*incY] = alpha * resLine + beta * Y[i*incY];
             }
         }
     } else if (TransA == MNCblasTrans || TransA == MNCblasConjTrans) {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1; i++) {
                 float resLine = 0.0;
                 for (int k=0; k<M; k++) {
-                    resLine += A[N*k+i]*X[k];
+                    resLine += A[N*k+i*incX]*X[k];
                 }
-                Y[j] = alpha * resLine + beta * Y[j];
+                Y[i*incY] = alpha * resLine + beta * Y[i*incY];
             }
         } else {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1 ; i++) {
                 float resLine = 0.0;
                 for (int k=0; k<M; k++) {
-                    resLine += A[M*i+k]*X[k];
+                    resLine += A[M*i*incX+k]*X[k];
                 }
-                Y[j] = alpha * resLine + beta * Y[j];
+                Y[i*incY] = alpha * resLine + beta * Y[i*incY];
             }
         }
     }
@@ -56,42 +60,46 @@ void mncblas_dgemv(MNCBLAS_LAYOUT layout,
                  const double *X, const int incX, const double beta,
                  double *Y, const int incY) {
 
-    register unsigned int i, j;
+    register unsigned int i;
 
     if (TransA == MNCblasNoTrans) {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < M ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < M/incX+1 ; i++) {
                 double resLine = 0.0;
                 for (int k=0; k<N; k++) {
-                    resLine += A[N*i+k]*X[k];
+                    resLine += A[N*i*incX+k]*X[k];
                 }
-                Y[j] = alpha * resLine + beta * Y[j];
+                Y[i*incY] = alpha * resLine + beta * Y[i*incY];
             }
         } else {
-            for (i = 0, j = 0 ; i < M ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < M /incX+1; i++) {
                 double resLine = 0.0;
                 for (int k=0; k<N; k++) {
-                    resLine += A[M*k+i]*X[k];
+                    resLine += A[M*k+i*incX]*X[k];
                 }
-                Y[j] = alpha * resLine + beta * Y[j];
+                Y[i*incY] = alpha * resLine + beta * Y[i*incY];
             }
         }
     } else if (TransA == MNCblasTrans || TransA == MNCblasConjTrans) {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1 ; i++) {
                 double resLine = 0.0;
                 for (int k=0; k<M; k++) {
-                    resLine += A[N*k+i]*X[k];
+                    resLine += A[N*k+i*incX]*X[k];
                 }
-                Y[j] = alpha * resLine + beta * Y[j];
+                Y[i*incY] = alpha * resLine + beta * Y[i*incY];
             }
         } else {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1 ; i++) {
                 double resLine = 0.0;
                 for (int k=0; k<M; k++) {
-                    resLine += A[M*i+k]*X[k];
+                    resLine += A[M*i*incX+k]*X[k];
                 }
-                Y[j] = alpha * resLine + beta * Y[j];
+                Y[i*incY] = alpha * resLine + beta * Y[i*incY];
             }
         }
     }
@@ -104,7 +112,7 @@ void mncblas_cgemv(MNCBLAS_LAYOUT layout,
                  const void *X, const int incX, const void *beta,
                  void *Y, const int incY) {
 
-    register unsigned int i, j;
+    register unsigned int i;
 
     complexe_float_t *x = (complexe_float_t *)X, *y = (complexe_float_t *)Y, *a = (complexe_float_t *)A;
     complexe_float_t *al = (complexe_float_t *)alpha, *be = (complexe_float_t *)beta;
@@ -112,56 +120,62 @@ void mncblas_cgemv(MNCBLAS_LAYOUT layout,
 
     if (TransA == MNCblasNoTrans) {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < M ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < M/incX+1 ; i++) {
                 complexe_float_t resLine = (complexe_float_t){0.0,0.0};
                 for (int k=0; k<N; k++) {
-                    resLine = add_complexe_float(resLine,mult_complexe_float(a[N*i+k],x[k]));
+                    resLine = add_complexe_float(resLine,mult_complexe_float(a[N*i*incX+k],x[k]));
                 }
-                y[j] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[j]));
+                y[i*incY] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[i*incY]));
             }
         } else {
-            for (i = 0, j = 0 ; i < M ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < M/incX+1 ; i++) {
                 complexe_float_t resLine = (complexe_float_t){0.0,0.0};
                 for (int k=0; k<N; k++) {
-                    resLine = add_complexe_float(resLine, mult_complexe_float(a[M*k+i], x[k]));
+                    resLine = add_complexe_float(resLine, mult_complexe_float(a[M*k+i*incX], x[k]));
                 }
-                y[j] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[j]));
+                y[i*incY] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[i*incY]));
             }
         }
     } else if (TransA == MNCblasTrans) {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1 ; i++) {
                 complexe_float_t resLine = (complexe_float_t){0.0,0.0};
                 for (int k=0; k<M; k++) {
-                    resLine = add_complexe_float(resLine, mult_complexe_float(a[N*k+i], x[k]));
+                    resLine = add_complexe_float(resLine, mult_complexe_float(a[N*k+i*incX], x[k]));
                 }
-                y[j] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[j]));
+                y[i*incY] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[i*incY]));
             }
         } else {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N; i++) {
                 complexe_float_t resLine = (complexe_float_t){0.0,0.0};
                 for (int k=0; k<M; k++) {
-                    resLine = add_complexe_float(resLine, mult_complexe_float(a[M*i+k], x[k]));
+                    resLine = add_complexe_float(resLine, mult_complexe_float(a[M*i*incX+k], x[k]));
                 }
-                y[j] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[j]));
+                y[i*incY] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[i*incY]));
             }
         }
     } else {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1 ; i++) {
                 complexe_float_t resLine = (complexe_float_t){0.0,0.0};
                 for (int k=0; k<M; k++) {
-                    resLine = add_complexe_float(resLine, mult_complexe_float(conj_complexe_float(a[N*k+i]), x[k]));
+                    resLine = add_complexe_float(resLine, mult_complexe_float(conj_complexe_float(a[N*k+i*incX]), x[k]));
                 }
-                y[j] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[j]));
+                y[i*incY] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[i*incY]));
             }
         } else {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1 ; i++) {
                 complexe_float_t resLine = (complexe_float_t){0.0,0.0};
                 for (int k=0; k<M; k++) {
-                    resLine = add_complexe_float(resLine, mult_complexe_float(conj_complexe_float(a[M*i+k]), x[k]));
+                    resLine = add_complexe_float(resLine, mult_complexe_float(conj_complexe_float(a[M*i*incX+k]), x[k]));
                 }
-                y[j] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[j]));
+                y[i*incY] = add_complexe_float(mult_complexe_float(alp, resLine), mult_complexe_float(bet, y[i*incY]));
             }
         }
     }
@@ -173,7 +187,7 @@ void mncblas_zgemv(MNCBLAS_LAYOUT layout,
                  const void *X, const int incX, const void *beta,
                  void *Y, const int incY) {
 
-    register unsigned int i, j;
+    register unsigned int i;
 
     complexe_double_t *x = (complexe_double_t *)X, *y = (complexe_double_t *)Y, *a = (complexe_double_t *)A;
     complexe_double_t *al = (complexe_double_t *)alpha, *be = (complexe_double_t *)beta;
@@ -181,56 +195,62 @@ void mncblas_zgemv(MNCBLAS_LAYOUT layout,
 
     if (TransA == MNCblasNoTrans) {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < M ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < M/incX+1 ; i++) {
                 complexe_double_t resLine = (complexe_double_t){0.0,0.0};
                 for (int k=0; k<N; k++) {
-                    resLine = add_complexe_double(resLine,mult_complexe_double(a[N*i+k],x[k]));
+                    resLine = add_complexe_double(resLine,mult_complexe_double(a[N*i*incX+k],x[k]));
                 }
-                y[j] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[j]));
+                y[i*incY] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[i*incY]));
             }
         } else {
-            for (i = 0, j = 0 ; i < M ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < M/incX+1 ; i++) {
                 complexe_double_t resLine = (complexe_double_t){0.0,0.0};
                 for (int k=0; k<N; k++) {
-                    resLine = add_complexe_double(resLine, mult_complexe_double(a[M*k+i], x[k]));
+                    resLine = add_complexe_double(resLine, mult_complexe_double(a[M*k+i*incX], x[k]));
                 }
-                y[j] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[j]));
+                y[i*incY] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[i*incY]));
             }
         }
     } else if (TransA == MNCblasTrans) {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1 ; i++) {
                 complexe_double_t resLine = (complexe_double_t){0.0,0.0};
                 for (int k=0; k<M; k++) {
-                    resLine = add_complexe_double(resLine, mult_complexe_double(a[N*k+i], x[k]));
+                    resLine = add_complexe_double(resLine, mult_complexe_double(a[N*k+i*incX], x[k]));
                 }
-                y[j] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[j]));
+                y[i*incY] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[i*incY]));
             }
         } else {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N ; i++) {
                 complexe_double_t resLine = (complexe_double_t){0.0,0.0};
                 for (int k=0; k<M; k++) {
-                    resLine = add_complexe_double(resLine, mult_complexe_double(a[M*i+k], x[k]));
+                    resLine = add_complexe_double(resLine, mult_complexe_double(a[M*i*incX+k], x[k]));
                 }
-                y[j] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[j]));
+                y[i*incY] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[i*incY]));
             }
         }
     } else {
         if (layout == MNCblasRowMajor) {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N/incX+1 ; i++) {
                 complexe_double_t resLine = (complexe_double_t){0.0,0.0};
                 for (int k=0; k<M; k++) {
-                    resLine = add_complexe_double(resLine, mult_complexe_double(conj_complexe_double(a[N*k+i]), x[k]));
+                    resLine = add_complexe_double(resLine, mult_complexe_double(conj_complexe_double(a[N*k+i*incX]), x[k]));
                 }
-                y[j] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[j]));
+                y[incY*i] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[i*incY]));
             }
         } else {
-            for (i = 0, j = 0 ; i < N ; i += incX, j += incY) {
+            #pragma omp parallel for
+            for (i = 0; i < N ; i++) {
                 complexe_double_t resLine = (complexe_double_t){0.0,0.0};
                 for (int k=0; k<M; k++) {
-                    resLine = add_complexe_double(resLine, mult_complexe_double(conj_complexe_double(a[M*i+k]), x[k]));
+                    resLine = add_complexe_double(resLine, mult_complexe_double(conj_complexe_double(a[M*i*incX+k]), x[k]));
                 }
-                y[j] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[j]));
+                y[i*incY] = add_complexe_double(mult_complexe_double(alp, resLine), mult_complexe_double(bet, y[i*incY]));
             }
         }
     }
